00c58827de5f0c353e91b914b3adfb3e195aea0c
Jxtopher <39927513+Jxtopher@users.noreply.github.com>
2025-04-13 16:58:26 +0200

    update

--- benches/first_improvement.rs
+++ benches/first_improvement.rs
@@ -1,23 +1,31 @@
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
-
+use criterion::{black_box, criterion_group, criterion_main, BenchmarkId, Criterion};
 use onemax::algorithm::Algorithm;
 use onemax::heuristics::FirstImprovement;
 use onemax::mutation_operator::Bitflip;
 use onemax::solution::Solution;
 
 pub fn criterion_benchmark(c: &mut Criterion) {
-    let mut solution = Solution::new(5);
+    let mut group = c.benchmark_group("Square Numbers");
 
-    let bitflip = Bitflip { p: 1.0 };
+    for &num in &[5, 10, 20] {
+        group.bench_with_input(BenchmarkId::from_parameter(num), &num, |b, &num| {
+            b.iter(|| {
+                let mut solution = Solution::new(num);
 
-    let first_improvement = FirstImprovement {
-        rounds: 5,
-        operator: Box::new(bitflip),
-    };
+                let bitflip = Bitflip { p: 1.0 };
 
-    c.bench_function("Frist inprovement x", |b| {
-        b.iter(|| first_improvement.apply(&mut solution))
-    });
+                let first_improvement = FirstImprovement {
+                    rounds: 5,
+                    operator: Box::new(bitflip),
+                };
+
+                // let start = Instant::now();
+                black_box(first_improvement.apply(&mut solution));
+            });
+        });
+    }
+
+    group.finish();
 }
 
 criterion_group!(benches, criterion_benchmark);
